53499ba5a5e5b49708d5643a8fff17be31a91953
arun789 <adwiti1975@gmail.com>
2017-05-08 00:10:37 -0700

    functions

--- MassUpload/WebContent/js/myScript.js
+++ MassUpload/WebContent/js/myScript.js
@@ -10,3 +10,24 @@ function sumofTwoNumbers(x, y) {
 	var z = x + y;
 	document.getElementById("sum").innerHTML = "z = " + z;
 }
+
+function objectValue() {
+	var person = {
+		firstName : "arun",
+		lastName : "singh",
+		age : 16,
+		eyeColor : "blue"
+	};
+	document.getElementById("demo").innerHTML = person.firstName + " : "
+			+ person.lastName + " is " + person.age
+			+ " years old with eye color as " + person.eyeColor;
+}
+
+function sumReturn(x, y) {
+	var z = x + y;
+	return z;
+}
+
+function displayinHTML(x, y) {
+	document.getElementById('demo').innerHTML = sumReturn(x, y);
+}
