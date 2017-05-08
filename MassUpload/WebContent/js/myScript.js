cfbca07211ad91c087f61beb4c5905336746a664
arun789 <adwiti1975@gmail.com>
2017-05-07 23:08:23 -0700

    Statement in javascript

--- MassUpload/WebContent/js/myScript.js
+++ MassUpload/WebContent/js/myScript.js
@@ -1,3 +1,12 @@
-function displayMe(){
+function displayMe() {
 	document.getElementById("demo").style.display = 'none';
+}
+
+function innerHTMLMethod() {
+	document.getElementById("demo").innerHTML = "value changed";
+}
+
+function sumofTwoNumbers(x, y) {
+	var z = x + y;
+	document.getElementById("sum").innerHTML = "z = " + z;
 }
